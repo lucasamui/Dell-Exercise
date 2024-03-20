f89e63c2def856b1501d7305c42cfbfc53b7e783
Lucas Amui <129434130+lucasamui@users.noreply.github.com>
2024-03-20 04:45:31 -0300

    Update apostador.cpp

---
+++ apostador.cpp
@@ -0,0 +1,10 @@
+#include "apostador.h"
+
+using namespace std;
+
+Apostador::Apostador(string _nome, string _cpf, vector<int> _numeros, int _id){
+  nome = _nome;
+  cpf = _cpf;
+  numeros = _numeros;
+  id = _id;
+}
